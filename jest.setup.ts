0fc1ab9ba8311549de2caeb6916f1c1f4a70ae99
Jonas Agnezi <jonas.agnezi@gmail.com>
2025-02-21 12:58:59 -0300

    test: add mock for tone.js globally

---
+++ jest.setup.ts
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom';
+
+export const toneTriggerAttackRelease = jest.fn();
+export const toneNow = jest.fn(() => 1234);
+
+jest.mock('tone', () => ({
+  Synth: jest.fn().mockImplementation(() => ({
+    toDestination: () => ({
+      triggerAttackRelease: toneTriggerAttackRelease,
+    }),
+  })),
+  now: toneNow
+}));
