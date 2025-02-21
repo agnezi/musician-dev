b5e752724731ad7df0a5fd94d32493eb530973a6
Jonas Agnezi <jonas.agnezi@gmail.com>
2025-02-21 14:27:15 -0300

    test: add tone start call

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,8 +2,10 @@ import '@testing-library/jest-dom';
 
 export const toneTriggerAttackRelease = jest.fn();
 export const toneNowMock = jest.fn(() => 1234);
+export const toneStartMock = jest.fn();
 
 jest.mock('tone', () => ({
+  start: toneStartMock,
   Synth: jest.fn().mockImplementation(() => ({
     toDestination: () => ({
       triggerAttackRelease: toneTriggerAttackRelease,
