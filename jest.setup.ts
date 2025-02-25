51fb4dc613049b3b9a95e0ac55d25d9bd24a0b3b
Jonas Agnezi <jonas.agnezi@gmail.com>
2025-02-24 21:58:03 -0300

    test: add mocks for polySynth

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,14 +1,23 @@
 import '@testing-library/jest-dom';
 
-export const toneTriggerAttackRelease = jest.fn();
+export const toneTriggerAttackReleaseMock = jest.fn();
+export const toneTriggerAttackMock = jest.fn();
+export const toneTriggerReleaseMock = jest.fn();
 export const toneNowMock = jest.fn(() => 1234);
 export const toneStartMock = jest.fn();
 
 jest.mock('tone', () => ({
   start: toneStartMock,
+  PolySynth: jest.fn().mockImplementation(() => ({
+    toDestination: () => ({
+      triggerAttack: toneTriggerAttackMock,
+      triggerRelease: toneTriggerReleaseMock,
+    }),
+  })),
   Synth: jest.fn().mockImplementation(() => ({
     toDestination: () => ({
-      triggerAttackRelease: toneTriggerAttackRelease,
+      triggerAttackRelease: toneTriggerAttackReleaseMock,
+      triggerAttack: toneTriggerAttackMock,
     }),
   })),
   now: toneNowMock,
