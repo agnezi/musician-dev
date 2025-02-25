d36840bdc27a88ad00e50e34abf930760b3989d0
Jonas Agnezi <jonas.agnezi@gmail.com>
2025-02-24 21:30:25 -0300

    feat: create default page

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import styles from './page.module.css';
+
+export default function Page() {
+  return (
+    <div className={styles.page}>
+      <header>
+        <h1>Links</h1>
+      </header>
+      <main className={styles.main}>
+        <ol>
+          <li>
+            <Link href={'/scales'}>Scales</Link>
+          </li>
+          <li>
+            <Link href={'/chords'}>Chords</Link>
+          </li>
+        </ol>
+      </main>
+    </div>
+  );
+}
